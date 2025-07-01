fccab45008201d1c21f250bec835a9f33fd5ad0b
“sp80808” <24103168@lcm.ac.uk>
2025-07-01 03:48:48 +0100

    feat: remove deprecated scripts and clean up project structure

---
+++ Blockchain_GUI/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Echochain Blockchain Management GUI</h1>
+        <p>Welcome to the Echochain management system.</p>
+        {/* Placeholder for future components */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
